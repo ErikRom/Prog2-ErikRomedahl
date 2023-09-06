5c6f84a25552bade5c4687611bf410b587651a06
ErikRom <erik.romedahl@gmail.com>
2023-09-06 10:48:17 +0200

    objektorientering

---
+++ Numeriska datatyper/objekt.py
@@ -0,0 +1,14 @@
+class Elev:
+    def __init__(self, namn, ålder, godkänd):
+        self.namn = namn
+        self.ålder = ålder
+        self.godkänd = godkänd
+    def presentera():
+        print(f"Hej! Jag heter {person1.namn}")
+
+person1 = Elev("Erik", 18, True)
+
+if person1.godkänd == True:
+    person1.glad = True
+
+Elev.presentera()
