6d51b00fc79c65a459d7088f999d12f6ac451949
ErikRom <erik.romedahl@gmail.com>
2023-10-04 10:49:52 +0200

    uppgifter

---
+++ Uppgifter/Aaah!.py
@@ -0,0 +1,10 @@
+def doctor(x, y):
+    if len(x) >= len(y):
+        print("go")
+    else:
+        print("no")
+
+x = input()
+y = input()
+
+doctor(x, y)
---
+++ Uppgifter/Övningar introduktion till ärvning.py
@@ -0,0 +1,34 @@
+class Djur():
+    def __init__(self, namn):
+        self.namn = namn
+
+class Fågel(Djur):
+    def __init__(self, namn, vingspann):
+        super.__init__(namn)
+        self.vingspann = vingspann
+
+class Fisk(Djur):
+    def __init__(self, namn, maxdjup):
+        super.__init__(namn)
+        self.maxdjup = maxdjup
+
+class Haj(Fisk):
+    def __init__(self, namn, maxdjup, antalTänder):
+        super.__init__(namn, maxdjup)
+        self.antalTänder = antalTänder
+
+class Torsk(Fisk):
+    def __init__(self, namn, maxdjup, hastighet):
+        super.__init__(namn, maxdjup)
+        self.hastighet = hastighet
+
+def fånga(haj, torsk):
+    if torsk.hastighet < 30 and haj.maxdjup >= torsk.maxdjup:
+        return True
+    else:
+        return False
+
+haj = Haj("Bert", 100, 80)
+torsk = Torsk("Nils", 90, 28)
+
+fånga(haj, torsk)
