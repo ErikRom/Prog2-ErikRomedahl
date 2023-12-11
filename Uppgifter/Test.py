ce4b823772b2f42e44099af3122e3649177e872d
ErikRom <erik.romedahl@gmail.com>
2023-12-11 15:21:46 +0100

    snake
    Co-authored-by: JackForsman <JackForsman@users.noreply.github.com>

--- Uppgifter/Test.py
+++ Uppgifter/Test.py
@@ -1,4 +1,25 @@
-i = i
+from tkinter import *
 
-for i == i + (i in range(1, (len(protein)) + 1))
-    
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 250
+SPACE_SIZE = 20
+BODY_PARTS = 5
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False)
+window.geometry('500x500')
+
+canvas_start = Canvas(window, bg='#FF00FF', height=500, width=500)
+canvas_start.pack()
+
+b = Button(window, text='START')
+b.pack()
+
+window.update()
+
+window.mainloop()
