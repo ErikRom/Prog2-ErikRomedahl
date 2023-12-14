888bcf5211f7c86f2152ffbc4a28896a0b6aeaa0
Erik <116350987+ErikRom@users.noreply.github.com>
2023-12-14 15:54:00 +0100

    Update Snake.py

--- Projekt/Snake.py
+++ Projekt/Snake.py
@@ -4,19 +4,27 @@ import random
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 50
-SPACE_SIZE = 20
+SPACE_SIZE = 30
 BODY_PARTS = 5
 SNAKE_COLOR = "#0863F9"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#19AE0D"
 
 def start():
-    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
-    canvas.place(x=0,y=0)
+    score = 0
 
-    label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
-    label.pack()
+    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT-50, width=GAME_WIDTH)
+    canvas.place(x=0,y=60)
 
+    canvas_top = Canvas(window, bg=('#000000'), height=60, width=GAME_WIDTH)
+    canvas_top.place(x=0, y=0)
+
+    label = Label(window, text="Score:{}".format(score), bg='#000000', fg='#FFFFFF', font=('consolas', 25))
+    label.place(x=10, y=10)
+
+    lable_highscore = Label(window, text="Highscore:{}".format(highscore), bg='#000000', fg='#FFFFFF', font=('consolas', 25))
+    lable_highscore.place(x=350, y=10)
+    
     window.update()
 
     class Snake:
@@ -126,15 +134,65 @@ def start():
 
 
     def game_over():
+        global highscore
 
-        canvas.delete(ALL)
-        canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
-                        font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
+        if score > highscore:
+            highscore = score
+
+        canvas.destroy()
+
+        canvas_gameover = Canvas(window, bg='#FF00FF', height=GAME_HEIGHT, width=GAME_WIDTH)
+        canvas_gameover.place(x=0,y=0)
+
+        canvas_gameover.create_text(350, 100, font=('Helvetica',70), text="GAME OVER", fill="red")
+        
+        b = Button(window, text="RESTART", height='5', width='20', command=start)
+        b.place(x=window_width/2-70, y=350)
+        
+        bu = Button(window, text='MENU', height='5', width='20', command=menu)
+        bu.place(x=window_width/2-70, y=500)
     
     snake = Snake()
     food = Food()
 
     next_turn(snake, food)
+
+
+def menu():
+    canvas_start = Canvas(window, bg='#FF00FF', height=GAME_HEIGHT, width=GAME_WIDTH)
+    canvas_start.place(x=0,y=0)
+
+    canvas_start.create_text(350, 100, font=('consalolas', 70), text='SNAKE', fill="#0863F9")
+    canvas_start.create_text(350, 650, font=('consalolas', 10), text='Av Erik Romedahl och Jack Forsman', fill="#000000")
+
+    b_food = Button(window, text='FOOD COLOR', height='5', width='20', command=food_color)
+    b_food.place(x=260, y=400)
+
+    b = Button(window, text='PLAY', height='5', width='20', command=start)
+    b.place(x=260, y=280)
+
+    window.update()
+
+
+def food_color():
+    canvas_food = Canvas(window, bg='#FF00FF', height=GAME_HEIGHT, width=GAME_WIDTH)
+    canvas_food.place(x=0,y=0)
+    
+    b_blue = Button(window, text='BLUE', height='5', width='20', command=menu)
+    b_blue.place(x=280, y=280)
+
+
+def change_direction(new_direction):
+    global direction
+    if new_direction in ['left', 'right', 'up', 'down']:
+        direction = new_direction
+
+
+def set_key_bindings():
+    window.bind('<Left>', lambda event: change_direction('left'))
+    window.bind('<Right>', lambda event: change_direction('right'))
+    window.bind('<Up>', lambda event: change_direction('up'))
+    window.bind('<Down>', lambda event: change_direction('down'))
 
 
 window = Tk()
@@ -143,19 +201,7 @@ window.title("Snake game")
 canvas_start = Canvas(window, bg='#FF00FF', height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas_start.pack()
 
-def place_button_center():
-    window_width = window.winfo_width()
-    window_height = window.winfo_height()
-    button_width = 200
-    button_height = 200
-    x = (window_width - button_width) / 2
-    y = (window_height - button_height) / 2
-    b.place(x=x, y=y)
-
-b = Button(window, text='START', height='10', width='20', bd='20', command=start)
-b.place(x=0, y=0)
-
-window.update()
+menu()
 
 window_width = window.winfo_width()
 window_height = window.winfo_height()
@@ -167,22 +213,10 @@ y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+score = 0
+highscore = 0
 direction = 'down'
 
-def change_direction(new_direction):
-    global direction
-    if new_direction in ['left', 'right', 'up', 'down']:
-        direction = new_direction
-
-def set_key_bindings():
-    window.bind('<Left>', lambda event: change_direction('left'))
-    window.bind('<Right>', lambda event: change_direction('right'))
-    window.bind('<Up>', lambda event: change_direction('up'))
-    window.bind('<Down>', lambda event: change_direction('down'))
-
-score = 0
-
 set_key_bindings()
-place_button_center()
 
 window.mainloop()
